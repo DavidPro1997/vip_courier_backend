e145010c3effb5fbf0ea98c43c3b1afca3c7c7df
DavidPro1997 <david_pro1997@outlook.com>
2024-11-08 17:49:51 -0500

    cd

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 class Config:
     DB_HOST = 'localhost'  # Cambia esto según tu configuración
-    DB_USER = 'vip_user'       # Cambia esto según tu configuración
-    DB_PASSWORD = 'courier'  # Cambia esto por tu contraseña
+    DB_USER = 'useradmin'       # Cambia esto según tu configuración
+    DB_PASSWORD = 'CaloloCNJ2024##'  # Cambia esto por tu contraseña
     DB_NAME = 'vipcourier'  # Nombre de tu base de datos
