ae36839413c98631d6fb18dc39665511d32829f8
DavidPro1997 <david_pro1997@outlook.com>
2024-11-11 09:26:50 -0500

    ambiente prod_prueba

--- app/config.py
+++ app/config.py
@@ -1,5 +1,12 @@
 class Config:
-    DB_HOST = 'localhost'  # Cambia esto según tu configuración
-    DB_USER = 'useradmin'       # Cambia esto según tu configuración
-    DB_PASSWORD = 'CaloloCNJ2024##'  # Cambia esto por tu contraseña
-    DB_NAME = 'vipcourier'  # Nombre de tu base de datos
+    # Produccion
+    DB_HOST = 'ec2-3-142-206-187.us-east-2.compute.amazonaws.com' 
+    DB_USER = 'useradmin'      
+    DB_PASSWORD = 'CaloloCNJ2024##' 
+    DB_NAME = 'vipcourier'  
+
+    # # Pruebas
+    # DB_HOST = 'localhost' 
+    # DB_USER = 'vip_user'      
+    # DB_PASSWORD = 'courier' 
+    # DB_NAME = 'vipcourier'  
