d4022b46ba349f8bec74b70f6704463c757f8964
DavidPro1997 <david_pro1997@outlook.com>
2024-11-11 09:38:13 -0500

    ambiente

---
+++ app/config.py
@@ -0,0 +1,12 @@
+class Config:
+    # Produccion
+    DB_HOST = 'ec2-3-142-206-187.us-east-2.compute.amazonaws.com' 
+    DB_USER = 'useradmin'      
+    DB_PASSWORD = 'CaloloCNJ2024##' 
+    DB_NAME = 'vipcourier'  
+
+    # # Pruebas
+    # DB_HOST = 'localhost' 
+    # DB_USER = 'vip_user'      
+    # DB_PASSWORD = 'courier' 
+    # DB_NAME = 'vipcourier'  
